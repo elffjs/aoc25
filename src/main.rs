9408fbb66377e73ad5b626d9be4f0c60e768423a
Dylan Moreland <dylan@dimo.zone>
2025-12-03 15:18:38 -0500

    Day 3

--- src/main.rs
+++ src/main.rs
@@ -123,6 +123,70 @@ fn day2_part2(input: &str) -> i64 {
     invalid
 }
 
+fn day3_part1(input: &str) -> i64 {
+    let mut joltage: i64 = 0;
+
+    for line in input.lines() {
+        let mut max: i64 = -1;
+
+        for i in 0..line.len() - 1 {
+            for j in i + 1..line.len() {
+                let together = format!("{}{}", &line[i..i + 1], &line[j..j + 1]);
+                let val: i64 = together.parse().unwrap();
+
+                if val > max {
+                    max = val;
+                }
+            }
+        }
+
+        joltage += max;
+    }
+
+    joltage
+}
+
+fn day3_part2(input: &str) -> i64 {
+    let mut joltage: i64 = 0;
+
+    for line in input.lines() {
+        let mut digit_idxs: [usize; 12] = [0; 12];
+        for digit_index in 0..12 {
+            let start = if digit_index == 0 {
+                0
+            } else {
+                digit_idxs[digit_index - 1] + 1
+            };
+            let stop = line.len() - 11 + digit_index;
+
+            let mut max_ind = start;
+            for i in start + 1..stop {
+                let max_val = line[max_ind..max_ind + 1].parse::<i64>().unwrap();
+                let val = line[i..i + 1].parse::<i64>().unwrap();
+
+                if val > max_val {
+                    max_ind = i;
+                }
+            }
+
+            digit_idxs[digit_index] = max_ind;
+        }
+
+        let mut out: String = String::new();
+        for digit_index in 0..12 {
+            out = format!(
+                "{}{}",
+                out,
+                &line[digit_idxs[digit_index]..digit_idxs[digit_index] + 1]
+            );
+        }
+
+        joltage += out.parse::<i64>().unwrap();
+    }
+
+    joltage
+}
+
 fn main() {
     let day1_input = fs::read_to_string("input1").unwrap();
     println!("D1P1: {}", day1_part1(&day1_input));
@@ -131,6 +195,10 @@ fn main() {
     let day2_input = fs::read_to_string("input2").unwrap();
     println!("D2P1: {}", day2_part1(&day2_input));
     println!("D2P2: {}", day2_part2(&day2_input));
+
+    let day3_input = fs::read_to_string("input3").unwrap();
+    println!("D3P1: {}", day3_part1(&day3_input));
+    println!("D3P2: {}", day3_part2(&day3_input));
 }
 
 #[cfg(test)]
@@ -158,5 +226,16 @@ L82";
         let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
         assert_eq!(1227775554, day2_part1(input));
         assert_eq!(4174379265, day2_part2(input));
+    }
+
+    #[test]
+    fn test_day3() {
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        assert_eq!(357, day3_part1(input));
+        assert_eq!(3121910778619, day3_part2(input));
+        // assert_eq!(4174379265, day2_part2(input));
     }
 }
